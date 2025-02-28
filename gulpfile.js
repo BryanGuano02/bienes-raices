faabba367fd960af00e5f0250949dbcb44aa7200
Bryan Guano <bryan.guano@epn.edu.ec>
2025-02-28 16:37:38 -0500

    feat(gulpfile): agregar tarea para la producción

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
 const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin'); // Minificar imagenes 
+const imagemin = require('gulp-imagemin'); // Minificar imagenes
 const notify = require('gulp-notify');
 const cache = require('gulp-cache');
 const clean = require('gulp-clean');
@@ -63,4 +63,5 @@ function watchArchivos() {
 
 exports.css = css;
 exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);
+exports.build = parallel(css, javascript, imagenes, versionWebp);
