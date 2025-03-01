5c4c2339a9b39416f25f759a8c5de3205b6351b2
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-01 11:18:31 -0500

    fix(gulpfile): corregir tarea de build

--- gulpfile.js
+++ gulpfile.js
@@ -69,4 +69,5 @@ function copyHTML() {
 exports.css = css;
 exports.watchArchivos = watchArchivos;
 exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);
+exports.build = parallel(css, javascript, imagenes, versionWebp);
 // exports.build = parallel(css, javascript, imagenes, versionWebp, copyHTML);
