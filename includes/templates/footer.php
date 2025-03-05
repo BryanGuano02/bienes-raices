0919ce92f95ee07c28ae497a5f72ceb9c8946ae0
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-05 15:13:48 -0500

    feat: crear foorter.php

---
+++ includes/templates/footer.php
@@ -0,0 +1,15 @@
+<footer class="footer seccion">
+    <div class="contenedor contenedor-footer">
+        <nav class="navegacion">
+            <a href="nosotros.php">Nosotros</a>
+            <a href="anuncios.php">Anuncios</a>
+            <a href="blug.php">Blog</a>
+            <a href="contacto.php">Contacto</a>
+        </nav>
+    </div>
+    <p class="copyright">Todos los derechos Reservados 2021 &copy;</p>
+</footer>
+<script src="js/bundle.min.js"></script>
+</body>
+
+</html>
