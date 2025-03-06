fc79380441f55b9f500d0b87e5a463e1319a79e3
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-06 13:49:12 -0500

    feat(formulario crear): crear formulario para nueva propiedad

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -8,8 +8,49 @@ incluirTemplate('header');
 
     <a href="../index.php" class="boton boton-verde">Volver</a>
 
-</main>
+    <form action="" class="formulario" method="GET" action="/admin/propiedades/crear.php">
+        <fieldset>
+            <legend>Información General</legend>
+            <label for="titulo">Título</label>
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
 
+            <label for="precio">Precio</label>
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
+
+            <label for="imagen">Imagen</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="descripcion">Descripción</label>
+            <textarea id="descripcion" name="descripcion"></textarea>
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Información Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones</label>
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="wc">Baños</label>
+            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="estacionamiento">Estacionamiento</label>
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select name="vendedor">
+                <option value="" disabled selected>-- Seleccione --</option>
+                <option value="1">Juan Perez</option>
+                <option value="2">Juan Perez</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" class="boton boton-verde" value="Crear Propiedad">
+    </form>
+</main>
 
 <?php
 incluirTemplate('footer');
