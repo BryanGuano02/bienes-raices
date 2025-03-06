0a39ed16d1dfb6005344f8021c6f5e27ff520ef9
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-06 14:36:49 -0500

    feat(formulario crear): añadir validación al formulario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,21 +5,76 @@ require 'includes/funciones.php';
 // BDs
 $db = conectarBD();
 
-incluirTemplate('header');
+$errores = [];
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $titulo = $_POST['titulo'];
     $precio = $_POST['precio'];
+    $descripcion = $_POST['descripcion'];
+    $habitaciones = $_POST['habitaciones'];
+    $wc = $_POST['wc'];
+    $estacionamiento = $_POST['estacionamiento'];
+    $vendedorId = isset($_POST['vendedorId']) ? $_POST['vendedorId'] : null;
 
+    if (!$titulo) {
+        $errores[] = "Debes añadir un título";
+    }
+
+    if (!$precio) {
+        $errores[] = "Debes añadir un precio";
+    }
+
+    if (strlen($descripcion) < 50) {
+        $errores[] = "Debes añadir una descripción y debe tener al menos 50 caracteres";
+    }
+
+    if (!$habitaciones) {
+        $errores[] = "Debes añadir el número de habitaciones";
+    }
+
+    if (!$wc) {
+        $errores[] = "Debes añadir el número de baños";
+    }
+
+    if (!$estacionamiento) {
+        $errores[] = "Debes añadir el número de estacionamientos";
+    }
+
+    // if (!$vendedorId) {
+    //     $errores[] = "Debes elegir el vendedor";
+    // }
+
+    // echo "<pre>";
+    // var_dump($errores);
+    // echo "</pre>";
+
+
+    if (empty($errores)) {
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId) VALUES ('$titulo','$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId')";
+
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            echo 'Registro exitoso';
+        }
+    }
 }
+
+incluirTemplate('header');
 ?>
 <main class="contenedor seccion">
     <h1>Crear</h1>
 
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
     <a href="../index.php" class="boton boton-verde">Volver</a>
 
-    <form action="" class="formulario" method="GET" action="/admin/propiedades/crear.php">
+    <form action="" class="formulario" method="POST" action="/admin/propiedades/crear.php">
         <fieldset>
             <legend>Información General</legend>
             <label for="titulo">Título</label>
@@ -52,7 +107,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <fieldset>
             <legend>Vendedor</legend>
 
-            <select name="vendedor">
+            <select name="vendedorId">
                 <option value="" disabled selected>-- Seleccione --</option>
                 <option value="1">Juan Perez</option>
                 <option value="2">Juan Perez</option>
