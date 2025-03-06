922353f7aa95c7d0b953333332ad1edcfcf2f460
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-06 12:39:53 -0500

    feat: crear estructura para CRUD simple

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php
+require 'includes/funciones.php';
+
+incluirTemplate('header');
+?>
+<main class="contenedor seccion">
+    <h1>Panel de Administración</h1>
+</main>
+
+
+<?php
+incluirTemplate('footer');
+?>
