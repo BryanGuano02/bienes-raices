8506510eb9e4f353aeee886e738d775ae69c9c09
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-07 12:57:56 -0500

    feat(formulario crear): agregar sanitización a los campos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -20,13 +20,13 @@ $vendedorId = '';
 $creado = date('Y/m/d');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $titulo = $_POST['titulo'];
-    $precio = $_POST['precio'];
-    $descripcion = $_POST['descripcion'];
-    $habitaciones = $_POST['habitaciones'];
-    $wc = $_POST['wc'];
-    $estacionamiento = $_POST['estacionamiento'];
-    $vendedorId = isset($_POST['vendedorId']) ? $_POST['vendedorId'] : null;
+    $titulo = mysqli_real_escape_string($db, $_POST['titulo']);
+    $precio = mysqli_real_escape_string($db, $_POST['precio']);
+    $descripcion = mysqli_real_escape_string($db, $_POST['descripcion']);
+    $habitaciones = mysqli_real_escape_string($db, $_POST['habitaciones']);
+    $wc = mysqli_real_escape_string($db, $_POST['wc']);
+    $estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+    $vendedorId = isset($_POST['vendedorId']) ? mysqli_real_escape_string($db, $_POST['vendedorId']) : null;
 
     if (!$titulo) {
         $errores[] = "Debes añadir un título";
