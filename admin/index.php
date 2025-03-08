4b1718ff1f3047662815b8e06071ee3b646e8be6
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-08 13:00:11 -0500

    feat: terminar crear y listar

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,60 @@
 <?php
-require 'includes/funciones.php';
+// Importar conexion
+require 'includes/config/database.php';
+$db = conectarBD();
 
+// Escribir el query
+$query = 'SELECT * FROM propiedades;';
+
+// Consultar la BD
+$resultadoConsulta  = mysqli_query($db, $query);
+
+// Mostrar resultados
+
+// Mensaje condicional
+$resultado= $_GET['resultado'] ?? null;
+
+// Importar funciones
+require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
 <main class="contenedor seccion">
     <h1>Panel de Administración</h1>
 
+    <?php if ($resultado === '1') { ?>
+        <p class="alerta exito">Anuncio creado correctamente</p>
+    <?php } ?>
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                <tr>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="imagen" class="imagen-tabla"></td>
+                    <td>$<?php echo $propiedad['precio']; ?></td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="propiedades/actualizar.php?id=<?php echo $propiedad['id'];?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 </main>
 
 
 <?php
+mysqli_close($db);
 incluirTemplate('footer');
 ?>
---
+++ admin/propiedades/actualizar.php
@@ -0,0 +1,164 @@
+<?php
+require 'includes/config/database.php';
+require 'includes/funciones.php';
+
+// BDs
+$db = conectarBD();
+
+$consulta = 'SELECT * FROM vendedores;';
+$resultado = mysqli_query($db, $consulta);
+
+$errores = [];
+
+$titulo = '';
+$precio = '';
+$descripcion = '';
+$habitaciones = '';
+$wc = '';
+$estacionamiento = '';
+$vendedorId = '';
+$creado = date('Y/m/d');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $titulo = mysqli_real_escape_string($db, $_POST['titulo']);
+    $precio = mysqli_real_escape_string($db, $_POST['precio']);
+    $descripcion = mysqli_real_escape_string($db, $_POST['descripcion']);
+    $habitaciones = mysqli_real_escape_string($db, $_POST['habitaciones']);
+    $wc = mysqli_real_escape_string($db, $_POST['wc']);
+    $estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+    $vendedorId = isset($_POST['vendedorId']) ? mysqli_real_escape_string($db, $_POST['vendedorId']) : null;
+    $imagen = isset($_FILES['imagen']) ? $_FILES['imagen'] : null;
+
+
+    if (!$titulo) {
+        $errores[] = "Debes añadir un título";
+    }
+
+    if (!$precio) {
+        $errores[] = "Debes añadir un precio";
+    }
+
+    if (strlen($descripcion) < 50) {
+        $errores[] = "Debes añadir una descripción y debe tener al menos 50 caracteres";
+    }
+
+    if (!$habitaciones) {
+        $errores[] = "Debes añadir el número de habitaciones";
+    }
+
+    if (!$wc) {
+        $errores[] = "Debes añadir el número de baños";
+    }
+
+    if (!$estacionamiento) {
+        $errores[] = "Debes añadir el número de estacionamientos";
+    }
+
+
+    if (!$vendedorId) {
+        $errores[] = "Debes elegir el vendedor";
+    }
+    if (!$imagen['name'] || $imagen['error']) {
+        $errores[] = 'La imagen es obligatoria';
+    }
+
+    $ancho = 1000;
+    $alto = 100;
+    $medida = $ancho * $alto;
+
+    if ($imagen['size'] > $medida) {
+        $errores[] = 'La imagen es demasiado grande';
+    }
+
+    if (empty($errores)) {
+        $nombreCarpeta = 'imagenes/';
+
+        $ruta = dirname(__DIR__, 2) . '/' . $nombreCarpeta;
+
+        if (!file_exists($ruta)) {
+            mkdir($ruta, 0777, true);
+        }
+
+        $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+
+        move_uploaded_file($imagen['tmp_name'], $ruta . $nombreImagen);
+
+        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ('$titulo','$precio', '$nombreImagen', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId')";
+
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            header('Location: /admin?resultado=1');
+        }
+    }
+}
+
+incluirTemplate('header');
+?>
+<main class="contenedor seccion">
+    <h1>Actualizar</h1>
+
+    <?php foreach ($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <a href="../index.php" class="boton boton-verde">Volver</a>
+
+    <form action="" class="formulario" method="POST" action="/admin/propiedades/crear.php"
+        enctype='multipart/form-data'>
+        <fieldset>
+            <legend>Información General</legend>
+            <label for="titulo">Título</label>
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value='<?php echo $titulo; ?>'>
+
+            <label for="precio">Precio</label>
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad"
+                value='<?php echo $precio; ?>'>
+
+            <label for="imagen">Imagen</label>
+            <input type="file" name="imagen" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="descripcion">Descripción</label>
+            <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Información Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones</label>
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9"
+                value='<?php echo $habitaciones; ?>'>
+
+            <label for="wc">Baños</label>
+            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value='<?php echo $wc; ?>'>
+
+            <label for="estacionamiento">Estacionamiento</label>
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9"
+                value='<?php echo $estacionamiento; ?>'>
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select name="vendedorId">
+                <option value="" disabled selected>-- Seleccione --</option>
+                <?php while ($vendedor = mysqli_fetch_assoc($resultado)): ?>
+
+                    <option <?php echo $vendedorId === $vendedor['id'] ? 'selected' : ''; ?>
+                        value="<?php echo $vendedor['id']; ?>">
+                        <?php echo $vendedor['nombre'] . ' ' . $vendedor['apellido']; ?>
+                    </option>
+                <?php endwhile ?>
+            </select>
+        </fieldset>
+
+        <input type="submit" class="boton boton-verde" value="Actualizar Propiedad">
+    </form>
+</main>
+
+<?php
+incluirTemplate('footer');
+?>
