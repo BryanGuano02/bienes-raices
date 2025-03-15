0da6d0984edb2a5df81d69e0165a1c8698b24b67
Bryan Guano <bryan.guano@epn.edu.ec>
2025-03-15 10:53:48 -0500

    feat(auth): autentificar crear

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,6 +1,12 @@
 <?php
-require 'includes/config/database.php';
 require 'includes/funciones.php';
+$auth = estaAutenticado();
+
+if (!$auth) {
+    header('Location: /');
+}
+
+require 'includes/config/database.php';
 
 // BDs
 $db = conectarBD();
