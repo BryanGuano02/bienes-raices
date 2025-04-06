76899532541ac49bf91f1c59bf550b9cf7dff115
Bryan Guano <alejoguano@hotmail.com>
2025-04-06 18:04:28 -0500

    feat: agregar PropiedadController

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+class PropiedadController {
+
+    public static function index() {
+        echo "indexando";
+    }
+    public static function crear() {
+
+        echo "creando";
+    }
+    public static function actualizar() {
+
+        echo "actualizando";
+    }
+}
