05d68542cc256312d5bb5c0ddb533c46bfc235d7
Bryan Guano <alejoguano@hotmail.com>
2025-04-06 18:04:52 -0500

    feat: agregar rutas para PropiedadController en index.php

--- public/index.php
+++ public/index.php
@@ -3,5 +3,13 @@
 require_once '../includes/app.php';
 
 use MVC\Router;
+use Controllers\PropiedadController;
 
 $router = new Router();
+
+$router->get('/admin', [PropiedadController::class, 'index']);
+$router->get('/propiedad/crear', [PropiedadController::class, 'crear']);
+$router->get('/propiedad/actualizar', [PropiedadController::class, 'actualizar']);
+
+
+$router->comprobarRutas();
