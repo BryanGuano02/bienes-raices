614d509ffc56a63884d712ff8beda77aee071ed8
Bryan Guano <alejoguano@hotmail.com>
2025-04-09 16:46:25 -0500

    feat: add POST routes for creating, updating, and deleting properties

--- public/index.php
+++ public/index.php
@@ -9,7 +9,10 @@ $router = new Router();
 
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedad/crear', [PropiedadController::class, 'crear']);
+$router->post('/propiedad/crear', [PropiedadController::class, 'crear']);
 $router->get('/propiedad/actualizar', [PropiedadController::class, 'actualizar']);
+$router->post('/propiedad/actualizar', [PropiedadController::class, 'actualizar']);
+$router->post('/propiedad/eliminar', [PropiedadController::class, 'eliminar']);
 
 
 $router->comprobarRutas();
