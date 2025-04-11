bb9fa2924d6db4375d6e293bc087f1e9c493ddf8
Bryan Guano <alejoguano@hotmail.com>
2025-04-11 12:16:11 -0500

    fix: implement eliminar functionality for vendedores and update layout in admin view

--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -30,12 +30,43 @@
                     <td><img src="/imagenes/<?php echo $propiedad->imagen; ?>" alt="imagen" class="imagen-tabla"></td>
                     <td>$<?php echo $propiedad->precio; ?></td>
                     <td>
-                        <form method="POST" class="w-100">
+                        <form method="POST" class="w-100" action="propiedad/eliminar">
                             <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
                             <input type="hidden" name="tipo" value="propiedad">
                             <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
                         <a href="/propiedad/actualizar?id=<?php echo $propiedad->id; ?>"
+                            class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <h2>Vendedores</h2>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Teléfono</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($vendedores as $vendedor): ?>
+                <tr>
+                    <td><?php echo $vendedor->id; ?></td>
+                    <td><?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td><?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id; ?>">
+                            <inout type="hidden" name="tipo" value="vendedor">
+                                <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="/admin/vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>"
                             class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
