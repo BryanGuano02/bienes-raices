f08de0796f9f32170725d11c243bc8ceb2f17420
Bryan Guano <alejoguano@hotmail.com>
2025-04-11 12:16:04 -0500

    fix: add vendedores to index and actualizar views, implement eliminar method

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -12,12 +12,14 @@ class PropiedadController {
     public static function index(Router $router) {
 
         $propiedades = Propiedad::getAll();
+        $vendedores = Vendedor::getAll();
         $resultado = $_GET['resultado'] ?? null;
 
         $router->render('/propiedades/admin',
             [
                 'propiedades' => $propiedades,
-                'resultado' => $resultado
+                'resultado' => $resultado,
+                'vendedores' => $vendedores
             ]
         );
     }
@@ -64,10 +66,51 @@ class PropiedadController {
         $errores = Propiedad::getErrores();
         $vendedores = Vendedor::getAll();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $args = $_POST['propiedad'];
+            $propiedad->sincronizar($args);
+
+            $errores = $propiedad->validar();
+
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+
+            if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                $manager = new ImageManager(new Driver());
+
+                // Abrir la imagen y redimensionarla
+                $imagen = $manager->read($_FILES['propiedad']['tmp_name']['imagen'])->cover(800, 600);
+                $propiedad->setImagen($nombreImagen);
+            }
+
+            if (empty($errores)) {
+                // Almacenar la imagen
+                if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                    $imagen->save(CARPETA_IMAGENES . $nombreImagen);
+                }
+
+                $propiedad->guardar();
+            }
+        }
+
         $router->render('propiedades/actualizar', [
             'propiedad' => $propiedad,
             'vendedores' => $vendedores,
             'errores' => $errores
         ]);
+    }
+
+    public static function eliminar() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+
+            if ($id) {
+                $tipo = $_POST['tipo'];
+                if (validarTipoContenido($tipo)) {
+                    $propiedad = Propiedad::get($id);
+                    $propiedad->eliminar();
+                }
+            }
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,14 +4,21 @@ require_once '../includes/app.php';
 
 use MVC\Router;
 use Controllers\PropiedadController;
+use Controllers\VendedorController;
 
 $router = new Router();
 
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedad/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedad/crear', [PropiedadController::class, 'crear']);
-$router->get('/propiedad/actualizar', [PropiedadController::class, 'actualizar']);
+$router->get('/propiedad/actualizar',  [PropiedadController::class, 'actualizar']);
 $router->post('/propiedad/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedad/eliminar', [PropiedadController::class, 'eliminar']);
+
+$router->get('/propiedad/crear', [VendedorController::class, 'crear']);
+$router->post('/propiedad/crear', [VendedorController::class, 'crear']);
+$router->get('/propiedad/actualizar',  [VendedorController::class, 'actualizar']);
+$router->post('/propiedad/actualizar', [VendedorController::class, 'actualizar']);
+$router->post('/propiedad/eliminar', [VendedorController::class, 'eliminar']);
 
 $router->comprobarRutas();
