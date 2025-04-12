674e2e21c8729deed5938d9dbe1083e2256e8e0a
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-12 10:40:57 -0500

    fix: update button label and reposition "Volver" link in actualizar view for consistency

--- views/propiedades/actualizar.php
+++ views/propiedades/actualizar.php
@@ -1,15 +1,15 @@
 <main class="contenedor seccion">
     <h1>Actualizar</h1>
 
-    <a href="../admin" class="boton boton-verde">Volver</a>
     <?php foreach ($errores as $error): ?>
         <div class="alerta error">
             <?php echo $error; ?>
         </div>
     <?php endforeach; ?>
 
+    <a href="../admin" class="boton boton-verde">Volver</a>
     <form class="formulario" method="POST" enctype='multipart/form-data'>
         <?php include 'formulario.php' ?>
-        <input type="submit" class="boton boton-verde" value="Crear Propiedad">
+        <input type="submit" class="boton boton-verde" value="Actualizar Propiedad">
     </form>
 </main>
