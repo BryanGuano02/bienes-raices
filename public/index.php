43a1f969f32f676d686b3cbadf893da4549e7f02
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-12 10:18:32 -0500

    fix: correct route definitions for VendedorController and update "Volver" link in crear view

--- public/index.php
+++ public/index.php
@@ -15,10 +15,10 @@ $router->get('/propiedad/actualizar',  [PropiedadController::class, 'actualizar'
 $router->post('/propiedad/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedad/eliminar', [PropiedadController::class, 'eliminar']);
 
-$router->get('/propiedad/crear', [VendedorController::class, 'crear']);
-$router->post('/propiedad/crear', [VendedorController::class, 'crear']);
-$router->get('/propiedad/actualizar',  [VendedorController::class, 'actualizar']);
-$router->post('/propiedad/actualizar', [VendedorController::class, 'actualizar']);
-$router->post('/propiedad/eliminar', [VendedorController::class, 'eliminar']);
+$router->get('/vendedor/crear', [VendedorController::class, 'crear']);
+$router->post('/vendedor/crear', [VendedorController::class, 'crear']);
+$router->get('/vendedor/actualizar',  [VendedorController::class, 'actualizar']);
+$router->post('/vendedor/actualizar', [VendedorController::class, 'actualizar']);
+$router->post('/vendedor/eliminar', [VendedorController::class, 'eliminar']);
 
 $router->comprobarRutas();
--- views/propiedades/crear.php
+++ views/propiedades/crear.php
@@ -1,13 +1,13 @@
 <main class="contenedor seccion">
     <h1>Crear</h1>
 
-    <a href="../admin" class="boton boton-verde">Volver</a>
     <?php foreach ($errores as $error): ?>
         <div class="alerta error">
             <?php echo $error; ?>
         </div>
     <?php endforeach; ?>
 
+    <a href="../admin" class="boton boton-verde">Volver</a>
     <form class="formulario" method="POST" enctype='multipart/form-data'>
         <?php include 'formulario.php' ?>
         <input type="submit" class="boton boton-verde" value="Crear Propiedad">
