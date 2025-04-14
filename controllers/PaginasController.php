dd474495d15805c0b90e6ed309e28d8d5a819431
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-14 11:07:42 -0500

    feat: implement renderIndex method in PaginasController for dynamic property rendering

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,9 +2,17 @@
 
 namespace Controllers;
 
+use Model\Propiedad;
+use MVC\Router;
+
 class PaginasController {
-    public static function renderIndex() {
-        echo 'Index';
+    public static function renderIndex(Router $router) {
+        $propiedades = Propiedad::getAnunciosLimite(3);
+
+        $router->render('paginas/index', [
+            'propiedades' => $propiedades
+        ]);
+
     }
 
     public static function renderNosotros() {
