2cf5223aad4ea53a8a34007ad1a1f46b76aee132
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-13 19:56:38 -0500

    feat: add PaginasController and routes for various pages

---
+++ controllers/PaginasController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController {
+    public static function renderIndex() {
+        echo 'Index';
+    }
+
+    public static function renderNosotros() {
+        echo 'Nosotros';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once '../includes/app.php';
 use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
+use Controllers\PaginasController;
 
 $router = new Router();
 
@@ -20,5 +21,14 @@ $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->get('/vendedores/actualizar',  [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
+
+$router->get('/', [PaginasController::class, 'renderIndex']);
+$router->get('/nosotros', [PaginasController::class, 'renderNosotros']);
+$router->get('/propiedades', [PaginasController::class, 'renderPropiedades']);
+$router->get('/propiedad', [PaginasController::class, 'renderPropiedad']);
+$router->get('/blog', [PaginasController::class, 'renderBlog']);
+$router->get('/entrada', [PaginasController::class, 'renderEntrada']);
+$router->get('/contacto', [PaginasController::class, 'renderContacto']);
+$router->get('/contacto', [PaginasController::class, 'renderContacto']);
 
 $router->comprobarRutas();
