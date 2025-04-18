d31c004d584c82fa30e4b8f36249cba5137bf6b4
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-18 14:25:40 -0500

    feat: add methods for rendering various pages in PaginasController

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -20,4 +20,29 @@ class PaginasController {
     public static function renderNosotros(Router $router) {
         $router->render('paginas/nosotros', []);
     }
+    public static function renderPropiedades(Router $router) {
+        $propiedades = Propiedad::getAll();
+
+        $router->render('paginas/propiedades', [
+            'propiedades' => $propiedades
+        ]);
+    }
+
+    public static function renderPropiedad(Router $router) {
+        $id = verificarId(('/propiedades'));
+        $propiedad = Propiedad::get($id);
+
+        $router->render('paginas/propiedad', [
+            'propiedad' => $propiedad
+        ]);
+    }
+    public static function renderBlog(Router $router) {
+        $router->render('paginas/blog');
+    }
+    public static function renderEntrada(Router $router) {
+        $router->render('paginas/entrada');
+    }
+    public static function renderContacto(Router $router) {
+        $router->render('paginas/contacto', []);
+    }
 }
