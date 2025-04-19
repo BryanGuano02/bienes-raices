c8105835f619b12a941bc2ffa363df60d90705c3
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-19 12:02:32 -0500

    feat: add dotenv configuration for environment variables in app.php

--- includes/app.php
+++ includes/app.php
@@ -2,6 +2,8 @@
 require 'funciones.php';
 require 'config/database.php';
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+$dotenv->load();
 
 $bd = conectarBD();
 
